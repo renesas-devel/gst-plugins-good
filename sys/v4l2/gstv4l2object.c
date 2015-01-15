fa7de346a5a04f02af71bc018bf21be8200aae9d
Hung Tran <hung.tran.jy@rvc.renesas.com>
2015-01-15 16:19:43 +0700

    Update the Copyright year
    Signed-off-by: Hung Tran <hung.tran.jy@rvc.renesas.com>

--- sys/v4l2/gstv4l2object.c
+++ sys/v4l2/gstv4l2object.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2001-2002 Ronald Bultje <rbultje@ronald.bitfreak.net>
  *               2006 Edgard Lima <edgard.lima@indt.org.br>
- *               2014 Renesas Electronics Corporation
+ *               2014-2015 Renesas Electronics Corporation
  *
  * gstv4l2object.c: base class for V4L2 elements
  *
